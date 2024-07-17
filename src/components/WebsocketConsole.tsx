6081cff490f6e14e7418bae76496cd6772773eb9
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-07-17 21:22:57 +0300

    Fix

--- src/components/WebsocketConsole.tsx
+++ src/components/WebsocketConsole.tsx
@@ -5,20 +5,14 @@ export default function WebsocketConsole({ location }: { location: string }) {
   const [logs, setLogs] = useState<string[]>([]);
 
   useEffect(() => {
-    setReadyState('Connecting removed');
+    setReadyState('');
   }, [location, setLogs, setReadyState]);
 
   return (
     <div className='mt-4 p-2'>
-      <h2 className='text-2xl'>Console</h2>
+      <h2 className='text-2xl' />
       <span>{readyState}</span>
-      <pre className='font-mono overflow-y-scroll h-[400px]'>
-        {logs.map((log, i) => (
-          <span key={i} className='mt-1 block'>
-            {log}
-          </span>
-        ))}
-      </pre>
+      <pre className='font-mono overflow-y-scroll h-[400px]'></pre>
     </div>
   );
 }
